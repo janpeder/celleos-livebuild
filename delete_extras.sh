2b1e0574adbfbedde11e15583f79ff65fe700af5
root <root@flaket>
2022-11-12 11:18:52 +0100

    added script to remove files added by script download_extras.sh

---
+++ delete_extras.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Delete GeoGebra:
+rm -r rawfilesinstall/opt/geogebra
+
+# Download GeoGebra icon:
+rm rawfilesinstall/usr/share/pixmaps/geogebra.png
+
+# Download Dash to Dock:
+rm -r rawfilesinstall/etc/skel/.local/share/gnome-shell/extensions/dash-to-dock\@micxgx.gmail.com
