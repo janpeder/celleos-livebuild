9adda15ad1cf63f19a1fbc3400e2b56c136ce3c7
root <root@flaket>
2023-09-21 07:33:35 +0200

    downloading libdvdcss first because that's always the troublesome one

--- download_extras.sh
+++ download_extras.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Download and build libdvdcss (for DVD decryption)
+INSTALLPATH=$(pwd)/rawfilesinstall/usr/local
+DOWNLOADURL=https://get.videolan.org/libdvdcss/1.4.2/libdvdcss-1.4.2.tar.bz2
+ARCHIVEFILE=libdvdcss-1.4.2.tar.bz2 
+DIRECTORY=libdvdcss-1.4.2/
+mkdir -p $INSTALLPATH
+wget $DOWNLOADURL
+tar xvfj $ARCHIVEFILE
+cd $DIRECTORY
+./configure --prefix=$INSTALLPATH
+make || exit
+make install || exit
+cd ..
+rm -r $DIRECTORY
+rm $ARCHIVEFILE
+
 # Download GeoGebra:
 wget https://download.geogebra.org/package/linux-port
 mkdir -p rawfilesinstall/opt
@@ -17,22 +33,6 @@ wget https://extensions.gnome.org/extension-data/dash-to-dockmicxgx.gmail.com.v6
 mkdir -p rawfilesinstall/etc/skel/.local/share/gnome-shell/extensions/dash-to-dock\@micxgx.gmail.com
 unzip dash-to-dockmicxgx.gmail.com.v69.shell-extension.zip -d rawfilesinstall/etc/skel/.local/share/gnome-shell/extensions/dash-to-dock\@micxgx.gmail.com/
 rm dash-to-dockmicxgx.gmail.com.v69.shell-extension.zip
-
-# Download and build libdvdcss (for DVD decryption)
-INSTALLPATH=$(pwd)/rawfilesinstall/usr/local
-DOWNLOADURL=https://get.videolan.org/libdvdcss/1.4.2/libdvdcss-1.4.2.tar.bz2
-ARCHIVEFILE=libdvdcss-1.4.2.tar.bz2 
-DIRECTORY=libdvdcss-1.4.2/
-mkdir -p $INSTALLPATH
-wget $DOWNLOADURL
-tar xvfj $ARCHIVEFILE
-cd $DIRECTORY
-./configure --prefix=$INSTALLPATH
-make || exit
-make install || exit
-cd ..
-rm -r $DIRECTORY
-rm $ARCHIVEFILE
 
 # Download Visual Studio Code:
 mkdir -p standalone-debs
